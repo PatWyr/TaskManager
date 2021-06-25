e6013f1809aaf325a97a8e11ee205ea1eca4cc2c
Patrick <74612989+PatWyr@users.noreply.github.com>
2021-06-25 16:56:59 +0200

    Done logic of calculator

--- src/main/java/Controller/CalculatorView.java
+++ src/main/java/Controller/CalculatorView.java
@@ -7,14 +7,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CalculatorView {
     private Stage stage = new Stage();
-
+    protected final Logger log = Logger.getLogger(getClass().getName());
     @FXML
     Button returnButton;
     @FXML
@@ -50,21 +52,119 @@ public class CalculatorView {
     @FXML
     TextArea screen = new TextArea();
     private float result = 0;
+    private String number = "";
+    private String all = "";
+    private int tmp = 0;
+    private String sign = "";
+    private int size = 0;
 
-
-    public void calculate() {
-
+    @FXML
+    public void calculate(ActionEvent actionEvent) {
+        number += ((Button)actionEvent.getSource()).getText();
+        all += ((Button)actionEvent.getSource()).getText();
+        log.log(Level.INFO,number);
+        screen.setText(String.valueOf(all));
     }
+
+    @FXML
+    public void add(ActionEvent actionEvent) {
+        tmp = Integer.parseInt(number);
+        all += " + ";
+        screen.setText(String.valueOf(all));
+        result += tmp;
+        number = "";
+        sign = "+";
+    }
+
+    @FXML
+    public void subtraction() {
+        size+=1;
+        tmp = Integer.parseInt(number);
+        all += " - ";
+        screen.setText(String.valueOf(all));
+        if(size == 1) {
+            result += tmp;
+        } else {
+            result -= tmp;
+        }
+        number = "";
+        sign = "-";
+    }
+
+    @FXML
+    public void multiplyOperation() {
+        size += 1;
+        tmp = Integer.parseInt(number);
+        all += " * ";
+        screen.setText(String.valueOf(all));
+        if(size == 1) {
+            result += tmp;
+        } else {
+            result *= tmp;
+        }
+        number = "";
+        sign = "*";
+    }
+
+    @FXML
+    public void divideOperation() throws InterruptedException {
+        size += 1;
+        tmp = Integer.parseInt(number);
+        all += " / ";
+        screen.setText(String.valueOf(all));
+        if(size == 1) {
+            result += tmp;
+        } else {
+            try {
+                result /= tmp;
+            } catch (ArithmeticException e){
+                e.getStackTrace();
+            } finally {
+                screen.setText("Err");
+            }
+        }
+        number = "";
+        sign = "/";
+    }
+
+
+
+    @FXML
+    public void sum(ActionEvent actionEvent) {
+        if(sign.contains("+")){
+            tmp = Integer.parseInt(number);
+            result += tmp;
+        } else if(sign.contains("-")){
+            tmp = Integer.parseInt(number);
+            result -= tmp;
+        } else if(sign.contains("*")){
+            tmp = Integer.parseInt(number);
+            result *= tmp;
+        } else if(sign.contains("/")){
+            tmp = Integer.parseInt(number);
+            result /= tmp;
+        }
+        screen.setText("= " + String.valueOf(result));
+        result = 0;
+        sign = "";
+    }
+
 
     @FXML
     public void initialize() {
         screen.setText(String.valueOf(result));
+        number = "";
+        size = 0;
     }
 
     @FXML
     public void reset(ActionEvent actionEvent) {
         result = 0;
         screen.setText(String.valueOf(result));
+        number = "";
+        size = 0;
+        tmp = 0;
+        all = "";
     }
 
     @FXML
