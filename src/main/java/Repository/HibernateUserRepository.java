56ddf2dcffa0d21f70464acf05225d847a95c168
Patrick <74612989+PatWyr@users.noreply.github.com>
2021-07-11 19:30:10 +0200

    HibernateUtil config

---
+++ src/main/java/Repository/HibernateUserRepository.java
@@ -0,0 +1,21 @@
+package Repository;
+
+import Model.User;
+
+public class HibernateUserRepository implements UserRepository {
+
+    @Override
+    public void saveUser() {
+
+    }
+
+    @Override
+    public void deleteUser() {
+
+    }
+
+    @Override
+    public User findUser(String login) {
+        return null;
+    }
+}
--- src/main/java/Repository/UserRepository.java
+++ src/main/java/Repository/UserRepository.java
@@ -1,4 +1,9 @@
 package Repository;
 
+import Model.User;
+
 public interface UserRepository {
+    void saveUser();
+    void deleteUser();
+    User findUser(String login);
 }
